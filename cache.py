3fbb3460103847ae55f811e3830167e83aba2333
TechnoCode44 <ElmoSmiley@outlook.com>
2023-07-23 11:28:32 +0100

    Added load_file_paths function

--- cache.py
+++ cache.py
@@ -2,3 +2,10 @@ def save_file_paths(files: list):
     with open("cache/files.txt", "w") as cache_file:
         for file in files:
             cache_file.write(f"{file}\n")
+
+def load_file_paths():
+    CACHE_FILE_PATH = "cache/files.txt"
+    cache_file = open(CACHE_FILE_PATH, "r").read()
+    file_paths = cache_file.split("\n")
+    file_paths = file_paths[:-1] # Last line is empty
+    return file_paths
