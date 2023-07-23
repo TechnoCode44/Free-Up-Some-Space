e1db96e33c564a14c0531834a95760c5a7a5b21b
TechnoCode44 <ElmoSmiley@outlook.com>
2023-07-23 11:58:45 +0100

    Add save_file_data function

--- cache.py
+++ cache.py
@@ -1,3 +1,5 @@
+from json import dump
+
 def save_file_paths(files: list):
     with open("cache/files.cache", "w") as cache_file:
         for file in files:
@@ -9,3 +11,8 @@ def load_file_paths():
     file_paths = cache_file.split("\n")
     file_paths = file_paths[:-1] # Last line is empty
     return file_paths
+
+def save_file_data(file_data: list): # Function name needs to be plural
+
+    with open(f"cache/file_data.cache", "w", encoding="utf-8") as cache_file:
+        dump(file_data, cache_file, ensure_ascii=False, indent=4)
