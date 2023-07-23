0f8ba481cd113398c5cf5f5ebe6e0692df9b24da
TechnoCode44 <ElmoSmiley@outlook.com>
2023-07-23 11:33:05 +0100

    Cache files now use the .cache file extension

--- cache.py
+++ cache.py
@@ -1,10 +1,10 @@
 def save_file_paths(files: list):
-    with open("cache/files.txt", "w") as cache_file:
+    with open("cache/files.cache", "w") as cache_file:
         for file in files:
             cache_file.write(f"{file}\n")
 
 def load_file_paths():
-    CACHE_FILE_PATH = "cache/files.txt"
+    CACHE_FILE_PATH = "cache/files.cache"
     cache_file = open(CACHE_FILE_PATH, "r").read()
     file_paths = cache_file.split("\n")
     file_paths = file_paths[:-1] # Last line is empty
